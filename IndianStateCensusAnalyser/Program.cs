f7ffcf611fff2e9963e290be2475c0e5fef59f34
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-30 22:45:11 +0530

    [Vedhashni] Updated the file with Indian State Census Analyser program

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
